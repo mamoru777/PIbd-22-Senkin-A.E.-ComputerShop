f6b71699fc440a40e69c7c49e728e3436d728d9f
Mamoru <senkinsaha@mail.ru>
2022-04-02 18:22:10 +0400

    Добавьте файлы проекта.

---
+++ ComputerShopContracts/BindingModels/PostavshikBindingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShopContracts.BindingModels
+{
+    public class PostavshikBindingModel
+    {
+        public int? Id { get; set; }
+        public string Login { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+    }
+}
